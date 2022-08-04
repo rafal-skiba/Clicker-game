1af1abc6ddf1e124726a022ee82467ec3f398e80
Rafał Skiba <rr.skiba92@gmail.com>
2022-08-04 19:33:43 +0200

    game click - finished

--- script.js
+++ script.js
@@ -11,23 +11,36 @@ function insertValue(value, placeID) {
     place.innerText = value;
 }
 
-
-
 let button = document.getElementById("the-button");
 let counter = 0;
-let timeLimit = 10;
+let timeLimit = 15;
+
 
 button.addEventListener("click", function() {
+
+
+
+
+
+
+
     counter++;
+
     let buttonWidth = button.offsetWidth;
     let buttonHeight = button.offsetHeight;
-    let leftRandom = getRandomNumber(0, button.parentElement.offsetWidth);
+    let leftRandom = getRandomNumber(80, button.parentElement.offsetWidth);
+    let topRandom = getRandomNumber(40, button.parentElement.offsetHeight);
 
-button.style.marginLeft = getNumberWithUnits(leftRandom - buttonWidth, "px");
+    button.style.marginLeft = getNumberWithUnits(leftRandom - buttonWidth, "px");
+    button.style.marginTop = getNumberWithUnits(topRandom - buttonHeight,"px");
+
+
     insertValue(counter, "score")
 
-} )
 
+ 
+
+} )
 
 
 let intervalID = setInterval(function () {
